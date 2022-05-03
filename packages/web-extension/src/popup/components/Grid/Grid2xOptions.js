8bfd3b2d44c9803031b1dcffb28fddc39436b871
Joe Kovach <kovachj@mskcc.org>
2022-05-03 11:25:20 -0400

    chore: remove grid options

--- packages/web-extension/src/popup/components/Grid/Grid2xOptions.js
+++ packages/web-extension/src/popup/components/Grid/Grid2xOptions.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import settings from 'carbon-components/es/globals/js/settings';
-import Checkbox from 'carbon-components-react/es/components/Checkbox';
 import FormGroup from 'carbon-components-react/es/components/FormGroup';
 import NumberInput from 'carbon-components-react/es/components/NumberInput';
 import ContentSwitcher from 'carbon-components-react/es/components/ContentSwitcher';
@@ -44,50 +43,6 @@ function Grid2xOptions({ disabled }) {
 
   return !onLoad ? null : (
     <FormGroup legendText="Options">
-      <div className={`${prefix}--row`}>
-        <div className={`${prefix}--col-sm-2`}>
-          <Checkbox
-            disabled={disabled}
-            labelText={labelMaker('Columns')}
-            id="toggle2xColumns"
-            checked={toggle2xGridOptions['toggle2xColumns']}
-            onChange={(e) => {
-              const changes = { ...toggle2xGridOptions };
-              changes['toggle2xColumns'] = e;
-              setToggle2xGridOptions(changes);
-              gaConfigurationEvent('2x-grid-change', '2x-columns', e);
-            }}
-          />
-        </div>
-        <div className={`${prefix}--col-sm-2`}>
-          <Checkbox
-            disabled={disabled}
-            labelText={labelMaker('Gutters')}
-            id="toggle2xGutters"
-            checked={toggle2xGridOptions['toggle2xGutters']}
-            onChange={(e) => {
-              const changes = { ...toggle2xGridOptions };
-              changes['toggle2xGutters'] = e;
-              setToggle2xGridOptions(changes);
-              gaConfigurationEvent('2x-grid-change', '2x-gutters', e);
-            }}
-          />
-        </div>
-        <div className={`${prefix}--col-sm-2`}>
-          <Checkbox
-            disabled={disabled}
-            labelText={labelMaker('Borders')}
-            id="toggle2xBorders"
-            checked={toggle2xGridOptions['toggle2xBorders']}
-            onChange={(e) => {
-              const changes = { ...toggle2xGridOptions };
-              changes['toggle2xBorders'] = e;
-              setToggle2xGridOptions(changes);
-              gaConfigurationEvent('2x-grid-change', '2x-borders', e);
-            }}
-          />
-        </div>
-      </div>
       <div className={`${prefix}--row`}>
         <div className={`${prefix}--col-sm-4`}>
           <FormLabel className={disabled ? `${prefix}--label--disabled` : ''}>
