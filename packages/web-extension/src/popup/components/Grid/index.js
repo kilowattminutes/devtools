13cb0ea1e17555b2659cfc123f035eabf20d2135
Joe Kovach <kovachj@mskcc.org>
2022-05-04 09:56:19 -0400

    chore: radio group

--- packages/web-extension/src/popup/components/Grid/index.js
+++ packages/web-extension/src/popup/components/Grid/index.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import settings from 'carbon-components/es/globals/js/settings';
-import Toggle from 'carbon-components-react/es/components/Toggle';
+//import settings from 'carbon-components/es/globals/js/settings';
+import RadioButtonGroup from 'carbon-components-react/es/components/RadioButtonGroup';
+import RadioButton from 'carbon-components-react/es/components/RadioButton';
 import { setStorage } from '@carbon/devtools-utilities/src/setStorage';
 import { getStorage } from '@carbon/devtools-utilities/src/getStorage';
-import { gaConfigurationEvent } from '@carbon/devtools-utilities/src/ga';
+//import { gaConfigurationEvent } from '@carbon/devtools-utilities/src/ga';
 import { gridVersions } from '../../../globals/options';
 import { defaults } from '../../../globals/defaults';
 
-const { prefix } = settings;
+//const { prefix } = settings;
 
-function Grid({ disabled }) {
+function Grid() {
   const [toggleGrids, setToggleGrids] = useState(defaults.grid);
   const [setGridVersionTitle] = useState(
     gridVersions[Object.keys(gridVersions)[0]]
@@ -43,134 +44,14 @@ function Grid({ disabled }) {
   });
 
   return !onLoad ? null : (
-    <>
-      <section className={`${prefix}--popup-main__section`}>
-        <div className={`${prefix}--row`}>
-          <div className={`${prefix}--col-sm-2`}>
-            <h2 className={`${prefix}--popup-main__section-title`}>Bookworm</h2>
-          </div>
-          <div className={`${prefix}--col-sm-2`}>
-            <Toggle
-              size="sm"
-              className={`${prefix}--popup-main__section-toggle`}
-              disabled={disabled}
-              id="toggleBookworm"
-              toggled={toggleGrids['toggleBookworm']}
-              onToggle={(e) => {
-                const changes = { ...toggleGrids };
-                changes['toggleBookworm'] = e;
-                gaConfigurationEvent('toggle-bookworm', 'global', e);
-              }}
-            />
-          </div>
-        </div>
-      </section>
-      <section className={`${prefix}--popup-main__section`}>
-        <div className={`${prefix}--row`}>
-          <div className={`${prefix}--col-sm-2`}>
-            <h2 className={`${prefix}--popup-main__section-title`}>Potato</h2>
-          </div>
-          <div className={`${prefix}--col-sm-2`}>
-            <Toggle
-              size="sm"
-              className={`${prefix}--popup-main__section-toggle`}
-              disabled={disabled}
-              id="togglePotato"
-              toggled={toggleGrids['togglePotato']}
-              onToggle={(e) => {
-                const changes = { ...toggleGrids };
-                changes['togglePotato'] = e;
-                gaConfigurationEvent('toggle-potato', 'global', e);
-              }}
-            />
-          </div>
-        </div>
-      </section>
-      <section className={`${prefix}--popup-main__section`}>
-        <div className={`${prefix}--row`}>
-          <div className={`${prefix}--col-sm-2`}>
-            <h2 className={`${prefix}--popup-main__section-title`}>Sarge</h2>
-          </div>
-          <div className={`${prefix}--col-sm-2`}>
-            <Toggle
-              size="sm"
-              className={`${prefix}--popup-main__section-toggle`}
-              disabled={disabled}
-              id="toggleSarge"
-              toggled={toggleGrids['toggleSarge']}
-              onToggle={(e) => {
-                const changes = { ...toggleGrids };
-                changes['toggleSarge'] = e;
-                gaConfigurationEvent('toggle-sarge', 'global', e);
-              }}
-            />
-          </div>
-        </div>
-      </section>
-      <section className={`${prefix}--popup-main__section`}>
-        <div className={`${prefix}--row`}>
-          <div className={`${prefix}--col-sm-2`}>
-            <h2 className={`${prefix}--popup-main__section-title`}>Slink</h2>
-          </div>
-          <div className={`${prefix}--col-sm-2`}>
-            <Toggle
-              size="sm"
-              className={`${prefix}--popup-main__section-toggle`}
-              disabled={disabled}
-              id="toggleSlink"
-              toggled={toggleGrids['toggleSlink']}
-              onToggle={(e) => {
-                const changes = { ...toggleGrids };
-                changes['toggleSlink'] = e;
-                gaConfigurationEvent('toggle-slink', 'global', e);
-              }}
-            />
-          </div>
-        </div>
-      </section>
-      <section className={`${prefix}--popup-main__section`}>
-        <div className={`${prefix}--row`}>
-          <div className={`${prefix}--col-sm-2`}>
-            <h2 className={`${prefix}--popup-main__section-title`}>Bullseye</h2>
-          </div>
-          <div className={`${prefix}--col-sm-2`}>
-            <Toggle
-              size="sm"
-              className={`${prefix}--popup-main__section-toggle`}
-              disabled={disabled}
-              id="toggleBullseye"
-              toggled={toggleGrids['toggleBullseye']}
-              onToggle={(e) => {
-                const changes = { ...toggleGrids };
-                changes['toggleBullseye'] = e;
-                gaConfigurationEvent('toggle-bullseye', 'global', e);
-              }}
-            />
-          </div>
-        </div>
-      </section>
-      <section className={`${prefix}--popup-main__section`}>
-        <div className={`${prefix}--row`}>
-          <div className={`${prefix}--col-sm-2`}>
-            <h2 className={`${prefix}--popup-main__section-title`}>Buster</h2>
-          </div>
-          <div className={`${prefix}--col-sm-2`}>
-            <Toggle
-              size="sm"
-              className={`${prefix}--popup-main__section-toggle`}
-              disabled={disabled}
-              id="toggleBuster"
-              toggled={toggleGrids['toggleBuster']}
-              onToggle={(e) => {
-                const changes = { ...toggleGrids };
-                changes['toggleBuster'] = e;
-                gaConfigurationEvent('toggle-buster', 'global', e);
-              }}
-            />
-          </div>
-        </div>
-      </section>
-    </>
+    <RadioButtonGroup labelText="Grid version">
+      <RadioButton labelText="Potato" value="Potato" id="Potato" />
+      <RadioButton labelText="Bookworm" value="Bookworm" id="Bookworm" />
+      <RadioButton labelText="Sarge" value="Sarge" id="Sarge" />
+      <RadioButton labelText="Slink" value="Slink" id="Slink" />
+      <RadioButton labelText="Bullseye" value="Bullseye" id="Bullseye" />
+      <RadioButton labelText="Buster" value="Buster" id="Buster" />
+    </RadioButtonGroup>
   );
 }
 
